3051d74fb8cc5926fc8730352ad5c7b7898c9326
Dan Halverson <daniel.m.halverson@gmail.com>
2019-12-07 20:34:10 -0600

    initial commit

---
+++ cloud-hello-rest/src/test/java/com/halversondm/hello/CloudHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halversondm.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
