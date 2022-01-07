ce62c07e077ed408aef9900f0114f8dc35fb5566
Dan Halverson <daniel.m.halverson@gmail.com>
2022-01-06 19:17:07 -0600

    updates

--- cloud-hello-rest/src/main/java/com/halversondm/hello/CloudHelloApplication.java
+++ cloud-hello-rest/src/main/java/com/halversondm/hello/CloudHelloApplication.java
@@ -2,8 +2,10 @@ package com.halversondm.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CloudHelloApplication {
 
 	public static void main(String[] args) {
